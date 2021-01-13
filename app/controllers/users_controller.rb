2993cb54efa581d2ce5b9aa8658424aeb0451ad4
Nick Kuria <nkuria01@gmail.com>
2021-01-13 12:10:13 +0300

    worked on show method and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,18 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
+        @user = User.new(user_params)
+       
+        if @user.save
+             redirect_to 'users/show'
+        else
+            flash.notice = "Error"
+        end
+    end
+    def user_params
+        params.require(:user).permit(:name)
     end
     def show
+        @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users/new', to: 'users#new'
-  get 'users/show' => 'users#show'
-  get 'users/create' => 'users#create'
+  resources :users, only: [:new, :create, :show]
+  # get 'users/new', to: 'users#new'
+  # get 'users/show' => 'users#show'
+  # get 'users/create' => 'users#create'
 end
