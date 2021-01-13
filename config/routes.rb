df52e430576a7bce32cb0552bbd1f11bd4b52a71
Nick Kuria <nkuria01@gmail.com>
2021-01-13 11:44:53 +0300

    created the user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/new', to: 'users#new'
+  get 'users/show' => 'users#show'
+  get 'users/create' => 'users#create'
 end
