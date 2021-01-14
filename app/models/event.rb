21d230c24651d8fa74dff5be98d99e64ebb033c9
angel <albarros1973@gmail.com>
2021-01-14 11:27:09 +0100

    Update user view to show past and previous events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
 
-  scope :upcoming_events, -> { where(creator: @current_user).where(event_date >= Time.now) }
-  scope :upcoming_events, -> { where(creator: @current_user).where(event_date < Time.now) }
+  scope :upcoming_events, -> { where('event_date >= ?', Time.now) }
+  scope :previous_events, -> { where('event_date <  ?', Time.now) }
 end
