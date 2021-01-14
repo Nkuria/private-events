2d6f4c0ce72cc53c57fbe67a8e6b4514895b27a0
angel <albarros1973@gmail.com>
2021-01-14 14:22:53 +0100

    Add two tests to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   has_many :events_created, foreign_key: :creator_id, class_name: 'Event'
   has_many :invitations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user creation' do
+    it 'creates user correctly' do
+      @user = User.new(name: 'Peter')
+      expect(@user.valid?).to be true
+    end
+    it 'user name can not be empty' do
+      @user = User.new(name: '')
+      expect(@user.valid?).to be false
+    end
+  end
 end
