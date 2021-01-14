cb7bfa8496739bebac26ec546da787a5dd76d789
Nick Kuria <nkuria01@gmail.com>
2021-01-14 13:46:11 +0300

    Worked on the readme file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    if session[:user_id]
-      @current_user ||=
-        User.find(session[:user_id])
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
