5821a848ccb627bfa3a8a7b0c1293d3c198a4de1
Nick Kuria <nkuria01@gmail.com>
2021-01-18 10:03:59 +0300

    Corrected tests on event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
+  validates :location, presence: true
+  validates :event_date, presence: true
+  validates :description, presence: true
 
   scope :upcoming_events, -> { where('event_date >= ?', Time.now) }
   scope :previous_events, -> { where('event_date <  ?', Time.now) }
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  let(:user) { create(:user) }
-  let(:event) { user.events.build(description: 'event created by user') }
-  let(:event1) { build(:event) }
+  let(:user) { User.create(name: 'John') }
+  let(:event) { Event.create(description: 'house party', location: 'House', event_date: Time.now, creator: user) }
+  let(:event1) { Event.create(description: '') }
   context 'checks validity of an event and makes sure its created by a user' do
     it 'makes sure description is not empty' do
-      event.description = ''
-      expect(event.valid?).to be false
-    end
-    it 'makes sure description is not empty' do
-      event.description = 'First event created'
       expect(event.valid?).to be true
     end
+    it 'makes sure location is not empty' do
+      event.location = ''
+      expect(event).not_to be_valid
+    end
+    it 'makes sure event date and time is not empty' do
+      event.event_date = ''
+      expect(event).not_to be_valid
+    end
+    it 'makes sure description is not empty' do
+      event.description = ''
+      expect(event).not_to be_valid
+    end
     it 'makes sure the event has a user' do
-      expect(event1.valid?).to be false
+      expect(event1).not_to be_valid
     end
   end
 end
