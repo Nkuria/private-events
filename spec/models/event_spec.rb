3e9de14ffedacf65539785b60fe06ef53c49f076
Nick Kuria <nkuria01@gmail.com>
2021-01-18 08:42:56 +0300

    Added tests for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user)}
+  let(:event) {user.events.build(description: 'event created by user')}
+  let(:event1) {build(:event)}
+  context "checks validity of an event and makes sure its created by a user" do
+    it 'makes sure description is not empty' do
+      event.description = ''
+      expect(event.valid?).to be false
+    end
+    it 'makes sure description is not empty' do
+      event.description = 'First event'
+      expect(event.valid?).to be true
+    end
+    it 'makes sure the event has a user' do
+      expect(event1.valid?).to be false
+    end
+  end
 end
